d362978a2500e5fcbd57c979583a2556da5ff039
Jessica Castro <Jesica.castros@outlook.com>
2022-10-04 19:56:11 -0300

    [core] Adding home page with next auth, stripe, global theme, util formats and others

---
+++ src/pages/_app.tsx
@@ -0,0 +1,19 @@
+import { Session } from 'next-auth'
+import type { AppProps } from 'next/app'
+import { Header } from '../components/Header'
+import { SessionProvider } from 'next-auth/react'
+import '../styles/global.scss'
+
+function MyApp({ 
+  Component, 
+  pageProps: { session, ...pageProps },
+}: AppProps<{ session: Session }>) {
+  return (
+    <SessionProvider session={session}>
+      <Header />
+      <Component {...pageProps} />
+    </SessionProvider>
+  )
+}
+
+export default MyApp
