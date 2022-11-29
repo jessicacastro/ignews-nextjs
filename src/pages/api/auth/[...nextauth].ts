1a3489dc1e034d61782b937fee02f7857aaf8f84
Jessica Castro <jessica.castros@outlook.com>
2022-11-29 16:18:45 -0300

    [feature] Add user validation to not duplicate fauna insertions

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -14,15 +14,31 @@ export default NextAuth({
     })
   ],
   callbacks: {
-    async signIn({ user, account, profile }) {
+    async signIn({ user }) {
       const { email } = user;
 
       try {
         await fauna.query(
-          q.Create(
-            q.Collection('users'), {
-              data: { email }
-            }
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(
+                  q.Index('user_by_email'),
+                  q.Casefold(email)
+                )
+              )
+            ),
+            q.Create(
+              q.Collection('users'), {
+                data: { email }
+              }
+            ),
+            q.Get(
+              q.Match(
+                q.Index('user_by_email'),
+                q.Casefold(email)
+              )
+            )
           )
         )
 
