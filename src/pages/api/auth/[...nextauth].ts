9c113a7cb0d713b78cec6b123f76f01842bfd6cc
Jessica Castro <jessica.castros@outlook.com>
2022-11-29 16:09:35 -0300

    [DB] Adding fauna config and service to NextAuth

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,6 +1,10 @@
 import NextAuth from 'next-auth'
 import GithubProvider from 'next-auth/providers/github'
 
+import { query as q } from 'faunadb'
+
+import { fauna } from '../../../services/fauna'
+
 export default NextAuth({
   // Configure one or more authentication providers
   providers: [
@@ -8,5 +12,24 @@ export default NextAuth({
       clientId: process.env.GITHUB_CLIENT_ID,
       clientSecret: process.env.GITHUB_CLIENT_SECRET
     })
-  ]
+  ],
+  callbacks: {
+    async signIn({ user, account, profile }) {
+      const { email } = user;
+
+      try {
+        await fauna.query(
+          q.Create(
+            q.Collection('users'), {
+              data: { email }
+            }
+          )
+        )
+
+        return true
+      } catch {
+        return false
+      }
+    }
+  }
 })
